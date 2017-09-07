9bc8c9ffdb74f779eb87afe4bcbb09f0e747624f
Param-Harrison <paramanantham@live.com>
2017-09-07 10:37:19 +0300

    fix: animation issue in slides

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,57 +12,57 @@ var bespoke = require('bespoke'),
 
 $(function () {
   $('body').show();
-});
 
-// Bespoke.js
-var deck = bespoke.from('article', [
-  keys(),
-  touch(),
-  bullets('li, .bullet'),
-  backdrop(),
-  nebula(),
-  fullscreen(),
-  substeps(),
-  progress()
-]);
+  // Bespoke.js
+  var deck = bespoke.from('article', [
+    keys(),
+    touch(),
+    bullets('li, .bullet'),
+    backdrop(),
+    nebula(),
+    fullscreen(),
+    substeps(),
+    progress()
+  ]);
 
-// Virtual DOM
-deck.on('activate', function(event) {
-  if ($(event.slide).hasClass('virtual-dom')) {
-    var parent = $('.virtual-dom');
-    var title  = $('.virtual-dom-steps');
+  // Virtual DOM
+  deck.on('activate', function(event) {
+    if ($(event.slide).hasClass('virtual-dom')) {
+      var parent = $('.virtual-dom');
+      var title  = $('.virtual-dom-steps');
 
-    parent.removeClass('step-1');
-    parent.removeClass('step-2');
-    parent.removeClass('step-3');
-    parent.removeClass('step-4');
-    parent.removeClass('step-5');
-    title.html('');
+      parent.removeClass('step-1');
+      parent.removeClass('step-2');
+      parent.removeClass('step-3');
+      parent.removeClass('step-4');
+      parent.removeClass('step-5');
+      title.html('');
 
-    switch (parent.find('.active').length) {
-      case 0:
-        parent.addClass('step-1');
-        break
-      case 1:
-        parent.addClass('step-2');
-        title.html('1. New Virtual DOM Subtree');
-        break;
-      case 2:
-        parent.addClass('step-3');
-        title.html('2. Diff with the old one');
-        break;
-      case 3:
-        parent.addClass('step-4');
-        title.html('3. Minimal set of DOM mutations');
-        break;
-      case 4:
-        parent.addClass('step-5');
-        title.html('4. Apply changes');
-        break;
-      }
-  }
+      switch (parent.find('.active').length) {
+        case 0:
+          parent.addClass('step-1');
+          break
+        case 1:
+          parent.addClass('step-2');
+          title.html('1. New Virtual DOM Subtree');
+          break;
+        case 2:
+          parent.addClass('step-3');
+          title.html('2. Diff with the old one');
+          break;
+        case 3:
+          parent.addClass('step-4');
+          title.html('3. Minimal set of DOM mutations');
+          break;
+        case 4:
+          parent.addClass('step-5');
+          title.html('4. Apply changes');
+          break;
+        }
+    }
 
-  return true;
+    return true;
+  });
 });
 
 require('prism');
